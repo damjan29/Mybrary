81d8ea7dcf4cc7b52b5059c7c7d14127eeb9e6d0
Damjan <damjan.milosevic@codepixel.me>
2020-12-09 12:12:26 +0100

    Initial Author routes

---
+++ models/author.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const autorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('Author', autorSchema);
--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/authors')
 
 
-console.log(process.env.NODE_ENV);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -18,13 +26,9 @@ mongoose.connect(process.env.DATABASE_URL, {
 console.log("DB connected!");
 })
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
 
 
 app.use('/', indexRouter);
+app.use('/authors', authRouter);
 
 app.listen(process.env.PORT || 3000);
